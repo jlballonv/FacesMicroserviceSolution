b212d6ea627cb9247c5a61f2aaf93589f8571b95
Jose-Luis Ballon <jlballonv@gmail.com>
2023-01-10 18:30:33 -0500

    Add faces web mvc and shared lib

---
+++ src/Faces.Web/Faces.WebMvc/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using Faces.WebMvc.Models;
+using Faces.WebMvc.ViewModels;
+using MassTransit;
+using Messaging.InterfacesConstants.Commands;
+using Messaging.InterfacesConstants.Constants;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Faces.WebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IBusControl _busControl;
+
+        public HomeController(ILogger<HomeController> logger, IBusControl busControl)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _busControl = busControl ?? throw new ArgumentNullException(nameof(busControl));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegisterOrder() 
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> RegisterOrder(OrderViewModel model) 
+        {
+            MemoryStream memory= new MemoryStream();
+            using (var uploadedFile = model.File.OpenReadStream()) 
+            {
+                await uploadedFile.CopyToAsync(memory);
+            }
+            model.ImageData = memory.ToArray();
+            model.ImageUrl = model.File?.Name;
+            model.OrderId = Guid.NewGuid();
+            var sendToUri = new Uri($"{RabbitMqMassTransitConstants.RABBIT_MQ_URI}" +
+                                    $"{RabbitMqMassTransitConstants.REGISTER_ORDER_COMMAND_QUEUE}");
+
+            var endPoint = await _busControl.GetSendEndpoint(sendToUri);
+
+            await endPoint.Send<IRegisterOrderCommand>(
+                new 
+                {
+                    model.OrderId,
+                    model.UserEmail,
+                    model.ImageData,
+                    model.ImageUrl,
+                });
+
+            ViewData["OrderId"] = model.OrderId;
+            return View("Thanks");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/FacesSharedLib/Messaging.InterfacesConstants/Constants/RabbitMqMassTransitConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.InterfacesConstants.Constants
+{
+    public class RabbitMqMassTransitConstants
+    {
+        public const string RABBIT_MQ_URI = "rabbitmq://rabbitmq:/";
+        public const string Username = "guest";
+        public const string Password = "guest";
+        public const string REGISTER_ORDER_COMMAND_QUEUE = "register.order.command";
+
+    }
+}
